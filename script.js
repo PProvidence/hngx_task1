73a25d5a813fb2c1ca43a4d9200f8e18bc068eb2
PProvidence <114724813+PProvidence@users.noreply.github.com>
2023-09-12 14:02:52 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-function updateDayAndTime() {
-    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    const currentDate = new Date();
-    const currentDayOfWeek = daysOfWeek[currentDate.getUTCDay()];
-    const currentUTCTime = currentDate.getTime();
-    
-    // Update elements with the current day of the week and UTC time
-    document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent = 'Current Day of the Week: ' + currentDayOfWeek;
-    document.querySelector('[data-testid="currentUTCTime"]').textContent = 'Current UTC Time: ' + currentUTCTime + ' milliseconds';
+const elementCurrentDay = document.querySelector("#currentDayOfTheWeek")
+const elementCurrentUTCTime = document.querySelector("#currentUTCTime");
+
+const currentDate = new Date();
+
+function getCurrentDayOfTheWeek() {
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const currentDayOfTheWeek = daysOfWeek[currentDate.getDay()];
+    return currentDayOfTheWeek;
 }
 
-// Call the function to update the current day of the week and UTC time
-updateDayAndTime();
+elementCurrentDay.textContent = getCurrentDayOfTheWeek();
+
+setInterval(() => {
+    const currentDate = new Date();
+    const currentUTCTime = currentDate.getTime();
+    elementCurrentUTCTime.textContent = currentUTCTime
+}, 1000);
